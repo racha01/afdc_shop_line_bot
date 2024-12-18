41774fe2218ac3bbd82afcc03efbc193284ebc6d
racha01 <rachanon1203@gmail.com>
2024-12-18 17:16:05 +0700

    add ssl

--- run.py
+++ run.py
@@ -3,10 +3,13 @@ from app import create_app
 app = create_app()
 
 app.secret_key = 'REPLACE ME - this value is here as a placeholder.'
-
+context = (
+    '/etc/letsencrypt/live/afdc.shop.coding.infy.uk/fullchain.pem',
+    '/etc/letsencrypt/live/afdc.shop.coding.infy.uk/privkey.pem'
+)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', 
             port=443,
-            ssl_context=('cert.pem', 'private.pem'),
+            ssl_context=context,
             debug=True)
