ca74699bda14e328ef538508f6fe3ecf546c9ddd
racha01 <rachanon1203@gmail.com>
2024-12-19 13:46:11 +0700

    update

--- run.py
+++ run.py
@@ -3,13 +3,9 @@ from app import create_app
 app = create_app()
 
 app.secret_key = 'REPLACE ME - this value is here as a placeholder.'
-context = (
-    '/etc/letsencrypt/live/afdc.shop.coding.infy.uk/fullchain.pem',
-    '/etc/letsencrypt/live/afdc.shop.coding.infy.uk/privkey.pem'
-)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', 
-            port=443,
-            ssl_context=context,
+            port=500,
+            ssl_context=('cert.pem', 'private.pem'),
             debug=True)
