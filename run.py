f75e00a2f1805c11922de878e0f408b80cfb8fa7
racha01 <rachanon1203@gmail.com>
2024-12-19 13:57:41 +0700

    add port

--- run.py
+++ run.py
@@ -6,6 +6,6 @@ app.secret_key = 'REPLACE ME - this value is here as a placeholder.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', 
-            port=500,
+            port=5000,
             ssl_context=('cert.pem', 'private.pem'),
             debug=True)
